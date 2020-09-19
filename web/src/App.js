86307c62addd2d1bf94b4f3230d538ddbfafc6b8
Derek Swingley <swingley@gmail.com>
2020-09-19 11:53:59 -0700

    timeline skeleton with create react app

---
+++ web/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+import './sdForward.css'
+
+import Header from './components/Header'
+
+function App() {
+  return (
+    <div className='container'>
+      <Header />
+      <div className='content'>
+        <p>
+          SANDAG Economic Timeline:
+        </p>
+        <p>
+          Coming Soon!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ web/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import sdForwardLogo from '../sdForwardLogo.png'
+
+const Header = () => {
+  return (
+    <img
+      src={sdForwardLogo}
+      alt='San Diego Forward Logo'
+      className='logo'
+    />
+  )
+}
+
+export default Header
