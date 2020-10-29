f4b8760f18714422311e5b6bea49c8674bab1c93
Derek Swingley <swingley@gmail.com>
2020-10-28 21:53:52 -0700

    Switch context to hooks and clean up

--- web/src/App.js
+++ web/src/App.js
@@ -6,49 +6,29 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import Stage from "./components/Stage";
 import YearSelector from "./components/YearSelector";
-import TimelineContext from "./TimelineContext";
-import { IoIosPin } from "react-icons/io";
+import Categories from "./components/Categories";
 
 import { EventDataProvider } from './EventDataContext'
 
 function App() {
   return (
-    <TimelineContext.Consumer>
-      {({ toggleShowFilter, selectedDec, selectedSectors, updateResults, stageResults }) => (
-        <div className="container flex">
-          <Header />
-          <div className="content flex">
-            {/* <img src={backgroundLandscape} alt="san diego landscape" />
-            <img src={road} alt="stage" /> */}
-            <button className="categories button" onClick={toggleShowFilter}>
-              {" "}
-              <IoIosPin />
-              categories
-            </button>
-            <div className="column flex">
-              <div className="row">
-                {/* {showFilter ? <Sidebar /> : null} */}
-                <Sidebar
-                  selectedDec={selectedDec}
-                  selectedSectors={selectedSectors}
-                  updateResults={updateResults}
-                />
-                {/* <Stage
-                  selectedSectors={selectedSectors}
-                  events={stageResults}
-                /> */}
-                <EventDataProvider>
-                  <Stage />
-                </EventDataProvider>
-              </div>
-              <div className="row fixed-100">
-                <YearSelector />
-              </div>
+    <EventDataProvider>
+      <div className="container flex">
+        <Header />
+        <div className="content flex">
+          <Categories />
+          <div className="column flex">
+            <div className="row">
+              <Sidebar />
+              <Stage />
+            </div>
+            <div className="row fixed-100">
+              <YearSelector />
             </div>
           </div>
         </div>
-      )}
-    </TimelineContext.Consumer>
+      </div>
+    </EventDataProvider>
   );
 }
 
--- web/src/EventDataContext.js
+++ web/src/EventDataContext.js
@@ -5,15 +5,38 @@ import React, {
 } from 'react'
 import timelineService from './timelineService'
 
+const selectedSectorsDefault = ["Civic", "Mobility", "Military", "Political", "Tourism"]
+const keySectorsDefault = ["Civic", "Mobility", "Military", "Political", "Tourism"]
+const allSectorsDefault = [
+  "Healthcare",
+  "Tech",
+  "Manufacturing",
+  "Goods Movement",
+  "Tribal",
+  "Crossborder",
+  "Landuse",
+]
 const Context = createContext({
   data: [],
-  loaded: false
+  loaded: false,
+  selectedDec: 2010,
+  selectedSectors: [],
+  keySectors: [],
+  allSectors: [],
+  showAllSectors: true,
+  showFilter: false,
 })
 export default Context
 
 export const EventDataProvider  = ({children}) => {
   const [data, setData] = useState(null)
   const [loading, setLoading] = useState(false)
+  const [selectedDecade, setSelectedDecade] = useState(null)
+  const [selectedSectors, setSelectedSectors] = useState(selectedSectorsDefault)
+  const [keySectors, setKeySectors] = useState(keySectorsDefault)
+  const [allSectors, setAllSectors] = useState(allSectorsDefault)
+  const [showAllSectors, setShowAllSectors] = useState(true)
+  const [showFilter, setShowFilter] = useState(false)
 
   const getEventData = async () => {
     setLoading(true)
@@ -25,9 +48,58 @@ export const EventDataProvider  = ({children}) => {
     getEventData()
   }, [])
 
+  const updateSelectedSectors = (e, sector) => {
+    const { checked } = e.target
+    if (checked) {
+      if (selectedSectors.length === 5) {
+        // TODO:  shows a message in the UI for this
+        console.log("you can only select 5 sectors at a time");
+        return
+      } else {
+        setSelectedSectors([...selectedSectors, e.target.value])
+      }
+    }
+    if (!checked) {
+      const nextSectors = [ ...selectedSectors ]
+      const sectorToRemove = nextSectors.indexOf(sector)
+      nextSectors.splice(sectorToRemove, 1)
+      setSelectedSectors(nextSectors)
+    }
+  }
+
+  const updateShowFilter = () => {
+    console.log('toggling filter')
+    setShowFilter(!showFilter)
+  }
+
+  const updateShowAllSectors = () => {
+    setShowAllSectors(!showAllSectors)
+  }
+
+  const removeSector = (sector) => {
+    let index = selectedSectors.indexOf(sector);
+    let newSelected = [ ...selectedSectors ]
+    newSelected.splice(index, 1);
+    setSelectedSectors(newSelected)
+  }
+
+  const clearSelectedSectors = () => {
+    setSelectedSectors([])
+  }
+
   const value = {
     loading,
-    data
+    data,
+    showFilter,
+    showAllSectors,
+    selectedSectors,
+    keySectors,
+    allSectors,
+    updateSelectedSectors,
+    removeSector,
+    clearSelectedSectors,
+    updateShowFilter,
+    updateShowAllSectors
   }
 
   return (
