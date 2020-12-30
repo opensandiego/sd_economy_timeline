dd847e2b19acfd1bb4df382a7e8a48c18221d99f
Derek Swingley <swingley@gmail.com>
2020-12-30 09:52:43 -0800

    Popup improvements

--- web/src/components/Popup.js
+++ web/src/components/Popup.js
@@ -5,16 +5,18 @@ import './popup.css'
 const Popup = ({ category, year, description, setEventForPopup }) => {
 
   return (
-    <div className='eventPopup'>
-      <div className='close'>
-        <button onClick={() => {
-          setEventForPopup(null)
-        }}>
-          <VscChromeClose size={18} />
-        </button>
+    <div className='eventPopupWrapper' onClick={() => setEventForPopup(null)}>
+      <div className='eventPopup'>
+        <div className='close'>
+          <button onClick={() => {
+            setEventForPopup(null)
+          }}>
+            <VscChromeClose size={18} />
+          </button>
+        </div>
+        <div>{year}: {category}</div>
+        <div>{description || 'No description available.'}</div>
       </div>
-      <div>{year}: {category}</div>
-      <div>{description || 'No description available.'}</div>
     </div>
   )
 }
