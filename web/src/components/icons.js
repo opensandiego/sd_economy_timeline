82c8fd4201432f3f2baf7898fa7a08029329d0f7
Derek Swingley <swingley@gmail.com>
2021-02-15 21:41:25 -0800

    Scale icons down 20%

--- web/src/components/icons.js
+++ web/src/components/icons.js
@@ -16,23 +16,23 @@ import tourism from '../assets/tourism@3x.png'
 import tribal from '../assets/tribal nations@3x.png'
 
 export const iconInfo = {
-  fire: { path: fire, file: 'F.I.R.E@3x.png', category: 'F.I.R.E.', width: 53, height: 53 },
-  gdp: { path: gdp, file: 'GDP@3x.png', category: 'GDP', width: 56, height: 45 },
-  retail: { path: retail, file: 'Retail@3x.png', category: 'Retail', width: 47, height: 53 },
-  transportation: { path: transportation, file: 'Transportation@3x.png', category: 'Transportation', width: 51, height: 39 },
-  mobility: { path: transportation, file: 'Transportation@3x.png', category: 'Mobility', width: 51, height: 39 },
-  civic: { path: civic, file: 'civic@3x.png', category: 'Civic', width: 42, height: 56, verticalShift: 8 },
-  crossborder: { path: crossborder, file: 'crossborder@3x.png', category: 'CrossBorder', width: 48, height: 48 },
-  education: { path: education, file: 'education@3x.png', category: 'Education', width: 58, height: 35 },
-  global: { path: global, file: 'global@3x.png', category: 'Global', width: 52, height: 52 },
-  goodsmovement: { path: goodsmovement, file: 'goods movement@3x.png', category: 'Goods Movement', width: 55, height: 39 },
-  healthcare: { path: healthcare, file: 'healthcare@3x.png', category: 'Healthcare', width: 42, height: 42 },
-  landuse: { path: landuse, file: 'historic land use@3x.png', category: 'LU & EP', width: 63, height: 33 },
-  innovation: { path: innovation, file: 'innovation@3x.png', category: 'Innovation', width: 50, height: 53, verticalShift: 5 },
-  manufacturing: { path: manufacturing, file: 'manufacturing@3x.png', category: 'Manufacturing', width: 50, height: 50 },
-  military: { path: military, file: 'military@3x.png', category: 'Military', width: 60, height: 53, verticalShift: 5 },
-  tourism: { path: tourism, file: 'tourism@3x.png', category: 'Tourism', width: 50, height: 44 },
-  tribal: { path: tribal, file: 'tribal nations@3x.png', category: 'Tribal', width: 65, height: 51 }
+  fire: { path: fire, file: 'F.I.R.E@3x.png', category: 'F.I.R.E.', width: 42, height: 42 },
+  gdp: { path: gdp, file: 'GDP@3x.png', category: 'GDP', width: 45, height: 36 },
+  retail: { path: retail, file: 'Retail@3x.png', category: 'Retail', width: 38, height: 42 },
+  transportation: { path: transportation, file: 'Transportation@3x.png', category: 'Transportation', width: 41, height: 31 },
+  mobility: { path: transportation, file: 'Transportation@3x.png', category: 'Mobility', width: 41, height: 31 },
+  civic: { path: civic, file: 'civic@3x.png', category: 'Civic', width: 34, height: 45, verticalShift: 8 },
+  crossborder: { path: crossborder, file: 'crossborder@3x.png', category: 'CrossBorder', width: 38, height: 38 },
+  education: { path: education, file: 'education@3x.png', category: 'Education', width: 46, height: 28 },
+  global: { path: global, file: 'global@3x.png', category: 'Global', width: 41, height: 41 },
+  goodsmovement: { path: goodsmovement, file: 'goods movement@3x.png', category: 'Goods Movement', width: 44, height: 31 },
+  healthcare: { path: healthcare, file: 'healthcare@3x.png', category: 'Healthcare', width: 34, height: 34 },
+  landuse: { path: landuse, file: 'historic land use@3x.png', category: 'LU & EP', width: 50, height: 26 },
+  innovation: { path: innovation, file: 'innovation@3x.png', category: 'Innovation', width: 40, height: 42, verticalShift: 5 },
+  manufacturing: { path: manufacturing, file: 'manufacturing@3x.png', category: 'Manufacturing', width: 40, height: 40 },
+  military: { path: military, file: 'military@3x.png', category: 'Military', width: 48, height: 42, verticalShift: 5 },
+  tourism: { path: tourism, file: 'tourism@3x.png', category: 'Tourism', width: 40, height: 35 },
+  tribal: { path: tribal, file: 'tribal nations@3x.png', category: 'Tribal', width: 52, height: 41 }
 }
 const scale = 0.75
 Object.values(iconInfo).forEach(info => {
