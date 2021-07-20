3bbfdcb209b40422b064bea2d091620ff8eb6d06
Derek Swingley <swingley@gmail.com>
2021-07-19 21:05:36 -0700

    Region for star of india

--- web/src/components/regions.js
+++ web/src/components/regions.js
@@ -10,7 +10,8 @@ export const regions = {
   fighter: [690, 90, 750, 50],
   mission: [710, 220, 800, 140],
   kumeyaay: [860, 250, 970, 200],
-  jet: [275, 70, 340, 40]
+  jet: [275, 70, 340, 40],
+  ship: [275, 220, 325, 160],
 }
 
 export const descriptions = {
@@ -22,7 +23,8 @@ export const descriptions = {
   fighter: 'The San Diego region is home to Naval Air Station North Island, known as the “birthplace of Naval Aviation,” and Marine Corps Air Station Miramar, which served as a training base for Air Force and Marine pilots during World War II.',
   mission: 'Mission San Diego de Alcalá, built in 1769, known as the “Soul of San Diego” was the first Mission in California.',
   kumeyaay: 'The e’waa, a traditional dome shaped home of the Kumeyaay, indigenous peoples of the San Diego region, depicts the rich human history that dates back 12,000 years.',
-  jet: 'The San Diego region’s unique geography, prosperous innovation industries, and multicultural talent pool make it a hub for international trade and commerce. In 2019 the region had 35.8 million visitors.'
+  jet: 'The San Diego region’s unique geography, prosperous innovation industries, and multicultural talent pool make it a hub for international trade and commerce. In 2019 the region had 35.8 million visitors.',
+  ship: 'The shipped named "Star of India" was built in 1863 and is part of the Maritime Museum of San Diego.'
 }
 
 export const positions = {
@@ -34,5 +36,6 @@ export const positions = {
   fighter: 'top',
   mission: 'top',
   kumeyaay: 'top',
-  jet: 'top'
+  jet: 'top',
+  ship: 'top'
 }
