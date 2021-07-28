1c73e554b8df0dd660ecf2c6a2f5131da83a5cd8
Derek Swingley <swingley@gmail.com>
2021-07-27 20:01:15 -0700

    Display categories on the stage below button

--- web/src/components/Categories.js
+++ web/src/components/Categories.js
@@ -1,17 +1,40 @@
 import React from 'react'
+import { VscChromeClose } from "react-icons/vsc";
 import { FaFilter } from "react-icons/fa"
 import TimelineContext from '../TimelineContext'
 
 const Categories = () => {
   return (
     <TimelineContext.Consumer>
-      {({updateShowFilter}) => (
-        <button className="categories button" onClick={updateShowFilter}>
-          {" "}
-          <FaFilter />
-          Categories
-        </button>
-      )}
+      {({selectedSectors, removeSector, updateShowFilter}) => {
+        return (
+          <div className="fixed">
+            <button className="categories button" onClick={updateShowFilter}>
+              {" "}
+              <FaFilter />
+              Categories
+            </button>
+            {selectedSectors.length > 0 &&
+              <div className="category-quick-look">
+                <div className="category-quick-look-heading">Filtered by:</div>
+                <div className="category-quick-look-selected">
+                  {selectedSectors.map(sector => {
+                    return (
+                      <div
+                        key={`category-pill-${sector}`}
+                        onClick={(e) => removeSector(sector)}
+                      >
+                        <VscChromeClose />
+                        {sector}
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+            }
+          </div>
+        )
+      }}
     </TimelineContext.Consumer>
   )
 }
